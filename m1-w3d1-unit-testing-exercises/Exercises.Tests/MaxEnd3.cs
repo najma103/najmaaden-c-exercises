8bd8f97ef12de3926d0302ed39a48190c749a382
Najma Aden <najma.aden@gmail.com>
2017-01-29 23:28:43 -0500

    created unit test for MaxEnd3 class

---
+++ m1-w3d1-unit-testing-exercises/Exercises.Tests/MaxEnd3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exercises.Tests
+{
+    [TestClass]
+    public class MaxEnd3
+    {
+        Exercises.MaxEnd3 maxEnd3Test = new Exercises.MaxEnd3();
+
+        [TestMethod]
+        public void MaxEnd3Test()
+        {
+            CollectionAssert.AreEqual(new int[] { 11, 11, 11 }, maxEnd3Test.MakeArray(
+                new int[] { 11, 5, 9 }), "input was {11, 5, 9}");
+            CollectionAssert.AreEqual(new int[] { 3, 3, 3 }, maxEnd3Test.MakeArray(
+                new int[] { 2, 11, 3 }), "Input was [2, 11, 3]");
+            //MakeArray([2, 11, 3]) → [3, 3, 3]
+            CollectionAssert.AreEqual(new int[] { 3, 3, 3 }, maxEnd3Test.MakeArray(
+                new int[] { 2, 11, 3 }));
+        }
+
+    }
+}
