c98b27bbd232df7e5dc11002a60b110d50379e01
Najma Aden <najma.aden@gmail.com>
2017-01-30 02:02:01 -0500

    completed week 3, Day 1 exercise and lecture exercise

---
+++ m1-w3d1-unit-testing-exercises/Exercises.Tests/NonStartTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exercises.Tests
+{
+    [TestClass]
+    public class NonStartTest
+    {
+        /*
+         * GetPartialString("Hello", "There") → "ellohere"
+         * GetPartialString("java", "code") → "avaode"	
+         * GetPartialString("shotl", "java") → "hotlava"	
+         */
+        NonStart noneStarUnitTest = new NonStart();
+        [TestMethod]
+        public void NoneStartTestMethod()
+        {
+            Assert.AreEqual("ellohere", noneStarUnitTest.GetPartialString("Hello", "There"));
+            Assert.AreEqual("avaode", noneStarUnitTest.GetPartialString("java", "code"));
+            Assert.AreEqual("hotlava", noneStarUnitTest.GetPartialString("shotl", "java"));
+        }
+    }
+}
---
+++ m1-w3d1-unit-testing-exercises/Exercises.Tests/StringBitsTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exercises.Tests
+{
+    [TestClass]
+    public class StringBitsTest
+    {
+        //GetBits("Hello") → "Hlo"	
+        //GetBits("Hi") → "H"	
+        //GetBits("Heeololeo") → "Hello"	
+
+        StringBits BitsUnitTest = new StringBits();
+        [TestMethod]
+        public void StringBitsUnitTest()
+        {
+            Assert.AreEqual("Hlo", BitsUnitTest.GetBits("Hello"));
+            Assert.AreEqual("H", BitsUnitTest.GetBits("Hi"));
+            Assert.AreEqual("Hello", BitsUnitTest.GetBits("Heeololeo"));
+        }
+    }
+}
