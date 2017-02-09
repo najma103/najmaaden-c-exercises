af5fd73b985508fc3ce32563762b41c7a50b3c37
Najma Aden <najma.aden@gmail.com>
2017-02-09 02:58:49 -0500

    SOLUTION FOR WEEK 4 DAY 1 HOMEWORK EXERCISE

--- m1-w4d1-file-io-part1-exercises/QuizMaker/Program.cs
+++ m1-w4d1-file-io-part1-exercises/QuizMaker/Program.cs
@@ -2,14 +2,88 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace QuizMaker
 {
     class Program
     {
+        static string searchPhrase = String.Empty;
+        static string filePath = @"C:\WordSearch\sample-quiz-file.txt";
         static void Main(string[] args)
         {
+            List<string[]> quizBank = new List<string[]>(readAndLoadFile(filePath));
+            foreach (string[] item in quizBank)
+            {
+                string q = item[0];
+                Console.WriteLine(q);
+                string correctAnswer = "";
+                for (int i = 1; i < item.Length; i++)
+                {
+              
+                    if (item[i].Contains("*"))
+                    {
+                        correctAnswer = item[i].Substring(0, item.Length - 1);
+                        Console.WriteLine(i + ". " + correctAnswer);
+                    } else
+                    {
+                        Console.WriteLine(i + ". " + item[i]);
+                    }
+
+                }
+                Console.WriteLine("Please enter the correct Answer ");
+                string userAnswer = Console.ReadLine();
+                if (userAnswer == correctAnswer)
+                {
+                    Console.WriteLine("You are correct ");
+                } else
+                {
+                    Console.WriteLine("Sorry Better luck next time");
+                }
+                Console.ReadLine();
+            }
+
+        }
+
+        ///reads file and loads List collection with everyline in the file
+        static List<string[]> readAndLoadFile(string filePath)
+        {
+            bool srcExists = File.Exists(filePath);
+            // the individual lines in a collection.
+            List<string[]> allLines = new List<string[]>();
+            if (srcExists)
+            {
+
+                try
+                {
+                    using (StreamReader sr = new StreamReader(filePath))
+                    {
+                        // Read the file until the end of the stream is reached
+                        while (!sr.EndOfStream)
+                        {
+                            // Read in a single line
+                            string[] line = sr.ReadLine().Split('|');
+
+                            // Add each line of the file to allLines collection
+                            allLines.Add(line);
+
+                        } //go to the next line until the end is reached
+                    }
+                }
+                catch (IOException e) //catch a specific type of Exception
+                {
+                    Console.WriteLine("Error reading the file");
+                    Console.WriteLine(e.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error invalid FilePath given ");
+                Console.ReadKey();
+            }
+            return allLines;
+
         }
     }
 }
--- m1-w4d1-file-io-part1-exercises/WordSearch/Program.cs
+++ m1-w4d1-file-io-part1-exercises/WordSearch/Program.cs
@@ -2,21 +2,88 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace WordSearch
 {
     class Program
     {
+        static string filePath = String.Empty;
+        static string searchPhrase = String.Empty;
         static void Main(string[] args)
         {
-			//1. Ask the user for the search string
-			//2. Ask the user for the file path
-			//3. Open the file
-				//4. Loop through each line in the file
-					//5. If the line contains the search string, print it out along with its line number
-				
-			
+            //1. Ask the user for the search string
+            //2. Ask the user for the file path
+            //3. Open the file
+            //4. Loop through each line in the file
+            //5. If the line contains the search string, print it out along with its line number
+
+           
+            Console.WriteLine("Please enter the full filepath");
+            filePath = Console.ReadLine();
+            Console.WriteLine("Please enter your Search phrase");
+            searchPhrase = Console.ReadLine();
+
+            List<string> listOfLines = new List<string>(readAndLoadFile(filePath));
+            if (!String.IsNullOrEmpty(searchPhrase))
+            {
+                searchAndDisplay(listOfLines);
+                Console.ReadKey();
+            }
+
+        }
+
+        static void searchAndDisplay(List<string> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].ToLower().Contains(searchPhrase.ToLower()))
+                {
+                    Console.WriteLine(i + " . " + list[i]);
+             
+                }
+            }
+        }
+
+
+        ///reads file and loads List collection with everyline in the file
+        static List<string> readAndLoadFile(string filePath)
+        {
+            bool srcExists = File.Exists(filePath);
+            // the individual lines in a collection.
+            List<string> allLines = new List<string>();
+            if (srcExists)
+            {
+
+                try
+                {
+                    using (StreamReader sr = new StreamReader(filePath))
+                    {
+                        // Read the file until the end of the stream is reached
+                        while (!sr.EndOfStream)
+                        {
+                            // Read in a single line
+                            string line = sr.ReadLine();
+
+                            // Add each line of the file to allLines collection
+                            allLines.Add(line);
+
+                        } //go to the next line until the end is reached
+                    }
+                }
+                catch (IOException e) //catch a specific type of Exception
+                {
+                    Console.WriteLine("Error reading the file");
+                    Console.WriteLine(e.Message);
+                }
+            } else
+            {
+                Console.WriteLine("Error invalid FilePath given ");
+                Console.ReadKey();
+            }
+            return allLines;
+            
         }
     }
 }
