0eddf6759a15fea6312d55edbb649115e9ecdad7
Najma Aden <najma.aden@gmail.com>
2017-03-14 01:16:22 -0400

    completed wednesday week 8 homework

--- m3-w2d3-session-exercises/TechElevator.Web/Controllers/HomeController.cs
+++ m3-w2d3-session-exercises/TechElevator.Web/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.SessionState;
+using TechElevator.Web.Models;
 
 namespace TechElevator.Web.Controllers
 {
@@ -14,9 +16,32 @@ namespace TechElevator.Web.Controllers
             return View("Index");
         }
 
-        public ActionResult WhyUs()
+        public ActionResult Page1(Favorites model)
         {
-            return View("WhyUs");
+            
+            return View("Page1", model);
+        }
+        public ActionResult Page2(Favorites model)
+        {
+            Session["Color"] = model.FavoriteColor;
+            return View("Page2", model);
+        }
+
+        public ActionResult Page3(Favorites model)
+        {
+            Session["Food"] = model.FavoriteFood;
+            return View("Page3", model);
+        }
+        public ActionResult Summary(Favorites model)
+        {
+            Session["Season"] = model.FavoriteSeason;
+            Favorites favorites = new Favorites();
+            favorites.FavoriteColor = (string)Session["Color"];
+            favorites.FavoriteFood = (string)Session["Food"];
+            favorites.FavoriteSeason = (string)Session["Season"];
+                        
+         
+            return View("Summary", favorites);
         }
     }
 }
---
+++ m3-w2d3-session-exercises/TechElevator.Web/Models/Favorites.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TechElevator.Web.Models
+{
+    public class Favorites
+    {
+        public string FavoriteFood { get; set; }
+        public string FavoriteColor { get; set; }
+        public string FavoriteSeason { get; set; }
+        public string Session { get; set; }
+    }
+}
